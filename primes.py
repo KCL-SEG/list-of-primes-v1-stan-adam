511420e3985031c542fbe1c2203e6a06bf137e6f
Adam Stankiewicz <stan.adamm@gmail.com>
2023-09-28 19:01:48 +0100

    Print out specified amount of primes

--- primes.py
+++ primes.py
@@ -1,6 +1,19 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(number):
+    prime = True
+    for i in range(2, int(number / 2) + 1):
+        if number % i == 0:
+            prime = False
+            break
+    return prime
+
 def primes(number_of_primes):
     list = []
+    potential_prime = 2
+    while number_of_primes > len(list):
+        if isPrime(potential_prime):
+            list.append(potential_prime)
+        potential_prime += 1
     return list
